4bcc4bdb683b3f8467a1de21212fc127ec822255
prredred <prredred@PRREDRED-IN.in.oracle.com>
2020-05-18 12:21:09 +0530

    Added new changes in VerifyOutput script

---
+++ src/main/java/com/script1/VerifyOutput.java
@@ -0,0 +1,15 @@
+package com.script1;
+
+public class VerifyOutput {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		System.out.println("Updated Statement");
+		
+		
+
+	}
+
+}
